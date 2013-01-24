e682031ff31335ee4606efeb1fc0bdf42b3e5b84
Fanny Xie <flyingsnow1125@gmail.com>
2013-01-24 20:50:30 +0800

    Initial Commit

---
+++ RatingView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RatingView
+//
+//  Created by Fanny on 1/24/13.
+//  Copyright (c) 2013 Fanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
